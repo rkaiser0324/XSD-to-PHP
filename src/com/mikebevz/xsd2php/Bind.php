28054b5ceafbcebfb97347458efc2d84a8dad549
Rolf Kaiser <1090569+rkaiser0324@users.noreply.github.com>
2018-11-24 05:47:24 -0800

    First attempt at recursively finding the comments for an inherited property.

--- src/com/mikebevz/xsd2php/Bind.php
+++ src/com/mikebevz/xsd2php/Bind.php
@@ -2,13 +2,13 @@
 namespace com\mikebevz\xsd2php;
 /**
  * Copyright 2010 Mike Bevz <myb@mikebevz.com>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,20 +19,20 @@ require_once 'Common.php';
 
 /**
  * Bind XML to PHP binding
- * 
+ *
  * @author Mike Bevz <myb@mikebevz.com>
  * @version 0.0.5
  */
 class Bind extends Common {
-    
+
     //protected $dom;
     //protected $namespaces;
-    
+
     /**
      * Unmarshal XML string to corresponding PHP binding
-     * 
+     *
      * @param string $xml XML string to unmarshal
-     * 
+     *
      * @return object PHP binding object
      */
     public function unmarshal($xml) {
@@ -44,65 +44,76 @@ class Bind extends Common {
         $this->dom->loadXML($xml);
 
         $this->namespaces =  $this->getDocNamespaces($this->dom);
-        
+
         // Find targetNamespace
-        
+
         $xpath = new \DOMXPath($this->dom);
         $query = ".";
         $root = $xpath->query($query);
-        $ns = $code = ""; 
+        $ns = $code = "";
         foreach ($root as $rt) {
             list ($ns, $name) = $this->parseQName($rt->nodeName, true);
-        }   
-        
+        }
+
         $className = $this->urnToPhpName($ns)."\\".$name;
-        
+
         if (!class_exists($className)) {
             throw new \RuntimeException('Class '.$className. ' is not found. Make sure it was included');
         }
-        
+
         $binding = new $className();
-        
+
         return $this->bindXml($xml, $binding);
-        
+
         // Instantiate class
         // Assign corresponding nodes to properties/classes
         // return PHP binding
     }
-    
+
     /**
      * Bind XML file to model
      * @param string $xml   XML source
      * @param object $model PHP Model to bind to
-     * 
+     *
      * @return object PHP model
      */
     public function bindXml($xml, $model) {
-        
+
         //print_r($xml."\n ".get_class($model));
-        
+
         $this->dom = new \DOMDocument();
         $this->dom->loadXML($xml);
-        
+
         $refl  = new \ReflectionClass(get_class($model));
         $xpath = new \DOMXPath($this->dom);
-        
+
         $query = "child::*";
         $childs = $xpath->query($query);
-        
+
         foreach ($childs as $child) {
-            
-            list ($ns, $name) = $this->parseQName($child->nodeName, true);
+
+			$name = $child->nodeName;
+			if ($this->isQName($name))
+			{
+				list ($ns, $name) = $this->parseQName($child->nodeName, true);
+			}
+
+			//$className = get_class($model);
             //$className = $this->urnToPhpName($ns)."\\".$name;
             try {
-                $propertyDocs = $refl->getProperty($name)->getDocComment();
+
+				// I think you need to go up the class tree until you find a comment
+				$docs = $this->getComments($refl, $name);
+				$propertyDocs = $refl->getProperty($name)->getDocComment();
+				if ($propertyDocs != false)
+				{
+					$docs = $this->parseDocComments($propertyDocs);
+					$className = $docs['var'];
+				}
             } catch (\ReflectionException $e) {
                 throw new \RuntimeException($e->getMessage().". Class ".get_class($model));
             }
-            $docs = $this->parseDocComments($propertyDocs);
-            $className = $docs['var'];
-            
-            
+
             if ($this->hasChild($child)) {
                 if (property_exists($model, $name)) {
                     $doc = new \DOMDocument();
@@ -111,7 +122,7 @@ class Bind extends Common {
                 } else {
                     throw new \RuntimeException('Class'. get_class($model). ' does not have property '.$name);
                 }
-                
+
             } else {
                 if (!property_exists($model, $name)) {
                     throw new \RuntimeException("Model ".get_class($model)." does not have property ".$name);
@@ -123,12 +134,12 @@ class Bind extends Common {
                     $type = $docs['xmlType'];
                     print_r("Type: ". $type."\n");
                     if ($type == 'attribute') {
-                        $model->{$name} = $child->nodeValue;      
+                        $model->{$name} = $child->nodeValue;
                     } elseif ($type == 'element') {
-                        $model->{$name} = $child->nodeValue;  
+                        $model->{$name} = $child->nodeValue;
                     } // elseif ($type == 'value') {
                     //    $model->value = $child->nodeValue;
-                    //} 
+                    //}
                     else {
                         throw new \RuntimeException('Class '.$className.' does not exist');
                     }
@@ -138,36 +149,59 @@ class Bind extends Common {
                     $cModel->value = $child->nodeValue;
                     $model->{$name} = $cModel;
                 }
-                
+
             }
         }
-        
-        
+
+
         return $model;
-    }
-    
-    private function hasChild($node) {
+	}
+
+	private function getComments($refl, $name)
+	{
+		$docs = null;
+		$propertyDocs = $refl->getProperty($name)->getDocComment();
+		if (!$propertyDocs)
+		{
+			// Try the parent class
+			if ($refl->getParentClass())
+			{
+				$docs = $this->getComments($refl->getParentClass(), $name);
+			}
+			else
+			{
+				// No-op; this was the root class, so no luck
+			}
+		}
+		else
+		{
+			$docs = $this->parseDocComments($propertyDocs);
+		}
+		return $docs;
+	}
+
+    public function hasChild($node) {
         if ($node->hasChildNodes()) {
           foreach ($node->childNodes as $c) {
             if ($c->nodeType === XML_ELEMENT_NODE) {
                 return true;
-            } 
+            }
           }
          }
          return false;
     }
-    
+
     public function bindXmlRec($node, $dom, $modelName) {
         $model = '';
-        
+
         if (class_exists($modelName)) {
             $model = new $modelName();
         } else {
             throw new \RuntimeException('Class '.$modelName.' does not exist');
         }
-        
+
         $refl = new \ReflectionClass($model);
-        
+
         $xpath = new \DOMXPath($dom);
         $query = "child::*";
         $childs = $xpath->query($query, $node);
@@ -185,7 +219,7 @@ class Bind extends Common {
                     $docs = $this->parseDocComments($propertyDocs);
                     $type = $docs['xmlType'];
                     if ($type == 'attribute') {
-                        $model->{$child->nodeName} = $child->nodeValue;      
+                        $model->{$child->nodeName} = $child->nodeValue;
                     } else {
                     //print_r($propertyType);
                      throw new \RuntimeException('Class '.$child->nodeName.' does not exist');
@@ -198,10 +232,10 @@ class Bind extends Common {
                 }
             }
         }
-        
+
         return $model;
-        
+
     }
-    
-    
+
+
 }
